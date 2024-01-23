d16c9cbcc72e999a96befb9a764f28fa5b8ad010
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-23 08:43:32 +0100

    update code 3

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -10,52 +10,52 @@ const Hello = (req, res) => {
 }
 
 
-// const VerifyToken = (req, res) => {
-//     try {
-//         var accessToken = "RTQWWTVHBDEJHJKIKIKNDS12345YT";
-//         var token = req.query["hub.verify_token"];
-//         var challenge = req.query["hub.challenge"];
-
-//         console.log("Token from request:", token);
-//         console.log("Challenge from request:", challenge);
-
-//         if (challenge != null && token != null && token === accessToken) {
-//             console.log("Verification successful. Sending challenge...");
-//             res.send(challenge);
-//         } else {
-//             console.log("Verification failed. Sending 400 status.");
-//             res.status(400).send();
-//         }
-//     } catch (e) {
-//         console.error("Error in verification:", e);
-//         res.status(400).send();
-//     }
-// }
-
 const VerifyToken = (req, res) => {
     try {
-        let myToken = "stanislas";
-        let mode = req.query["hub.mode"];
-        let token = req.query["hub.verify_token"];
-        let challenge = req.query["hub.challenge"];
+        var accessToken = "stanislas";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
 
-        if (mode && token) {
-            if (mode === "subscribe" && token === myToken ) {
-                res.status(200).send(challenge);
-            } else {
-                res.status(403).send();
-                
-            }
-            
+        console.log("Token from request:", token);
+        console.log("Challenge from request:", challenge);
+
+        if (challenge != null && token != null && token === accessToken) {
+            console.log("Verification successful. Sending challenge...");
+            res.send(challenge);
+        } else {
+            console.log("Verification failed. Sending 400 status.");
+            res.status(400).send();
         }
-
-
-        
     } catch (e) {
         console.error("Error in verification:", e);
         res.status(400).send();
     }
 }
+
+// const VerifyToken = (req, res) => {
+//     try {
+//         let myToken = "stanislas";
+//         let mode = req.query["hub.mode"];
+//         let token = req.query["hub.verify_token"];
+//         let challenge = req.query["hub.challenge"];
+
+//         if (mode && token) {
+//             if (mode === "subscribe" && token === myToken ) {
+//                 res.status(200).send(challenge);
+//             } else {
+//                 res.status(403).send();
+                
+//             }
+            
+//         }
+
+
+        
+//     } catch (e) {
+//         console.error("Error in verification:", e);
+//         res.status(400).send();
+//     }
+// }
 
 
 const ReceivedMessage = (req, res) => {
