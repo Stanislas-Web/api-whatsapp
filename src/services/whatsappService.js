d760a0ee560de9ba93a19f8827b1672ec64a554a
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-23 08:48:01 +0100

    update code 4

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -10,7 +10,7 @@ function SendMessageWhatsApp(data){
         body: data,
         headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer EAAOKXpr3YjIBO5pAhduZBEVI449RPzVG3tVcO1tmCr4VzlkVwSgkpYRrh5AEeJwXDwiccCsOYac7AarAWfESdIeV1HwUijzktLUwQctrRNChyb03Qu2zoKwLS55lsLz5WEZC1d6JS8BlUQSXs9DZA9g1rhsps6vEgRc40x0umTEG9jThnNbZAbDZA1jxYQiYJ3CAEzAoAkZBswY5f4SpuY"
+            Authorization: "Bearer EAAOKXpr3YjIBO6rEBNbZBR7YmeknEYfmupvAPCtLX9af4wQ8sBPxs4w1Ykn12pUMbfZCU7CIH84j8chPEYqswURj2lkJuuNZAQv3sjzcslBDzXk7tyALOsFUsEX3oZB6h8h4wpqP7QcJw63UWHbw5DwjvHgedrW7w9ZBaOeufuOqy9JxdtZBwfCwfE2mDZBJqlyuxkysiieGv5JbvGZAgWIZD"
         }
     };
     const req = https.request(options, res => {
