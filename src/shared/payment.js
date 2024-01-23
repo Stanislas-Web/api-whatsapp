c3649b06a69af9855dd0bc29c98e22a56bc62868
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-23 17:40:48 +0100

    update code updatee  re  lfffrfy

--- src/shared/payment.js
+++ src/shared/payment.js
@@ -7,26 +7,25 @@ const FormData = require('form-data');
 async function payment(currency, provider, walletId, abonnement, phone) {
     try {
         let data = new FormData();
-        // data.append('currency', currency);
-        // data.append('provider', provider);
-        // data.append('walletID', walletId);
-        // data.append('etudiantID', 'STDTAC20230330092HFM5UM110173');
-        // data.append('abonnementID', abonnement);
-
         data.append('currency', currency);
         data.append('provider', provider);
         data.append('walletID', walletId);
-        data.append('IDgenerateEtud', 'STDTAC20230330092HFM5UM110173');
-        data.append('IDabonnement', abonnement);
-        data.append('IdAgent', "AGTTAC20230505086WCYT_9110211");
-        data.append('phoneEtud', phone);
+        data.append('etudiantID', 'STDTAC20230330092HFM5UM110173');
+        data.append('abonnementID', abonnement);
+
+        // data.append('currency', currency);
+        // data.append('provider', provider);
+        // data.append('walletID', walletId);
+        // data.append('IDgenerateEtud', 'STDTAC20230330092HFM5UM110173');
+        // data.append('IDabonnement', abonnement);
+        // data.append('IdAgent', "AGTTAC20230505086WCYT_9110211");
+        // data.append('phoneEtud', phone);
         
 
         let config = {
             method: 'post',
             maxBodyLength: Infinity,
-            // url: 'https://tag.trans-academia.cd/Api_abonnement.php',
-            url: 'https://tag.trans-academia.cd/Flash_Abonnement_wt.php',
+            url: 'https://tag.trans-academia.cd/Api_abonnement.php',
             headers: {
                 ...data.getHeaders()
             },
