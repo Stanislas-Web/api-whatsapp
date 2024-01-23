96d1d381bf8d8bf6f59ab9286ab4801b41c3225e
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-23 17:30:29 +0100

    update code updatee  re  lfffrf

--- src/shared/payment.js
+++ src/shared/payment.js
@@ -3,19 +3,28 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 
+
 async function payment(currency, provider, walletId, abonnement) {
     try {
         let data = new FormData();
+        // data.append('currency', currency);
+        // data.append('provider', provider);
+        // data.append('walletID', walletId);
+        // data.append('etudiantID', 'STDTAC20230330092HFM5UM110173');
+        // data.append('abonnementID', abonnement);
+
         data.append('currency', currency);
         data.append('provider', provider);
         data.append('walletID', walletId);
-        data.append('etudiantID', 'STDTAC20230330092HFM5UM110173');
-        data.append('abonnementID', abonnement);
+        data.append('IDgenerateEtud', 'STDTAC20230330092HFM5UM110173');
+        data.append('IDabonnement', abonnement);
+        data.append('IdAgent', "AGTTAC20230505086WCYT_9110211");
 
         let config = {
             method: 'post',
             maxBodyLength: Infinity,
-            url: 'https://tag.trans-academia.cd/Api_abonnement.php',
+            // url: 'https://tag.trans-academia.cd/Api_abonnement.php',
+            url: 'https://tag.trans-academia.cd/Flash_Abonnement_wt.php',
             headers: {
                 ...data.getHeaders()
             },
