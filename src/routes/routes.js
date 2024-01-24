19fa2e8a8c793cfc90f11e4d5efa56f69aa59980
Stanislas-Web <stamislasmakengo1@gmail.com>
2024-01-24 22:08:12 +0100

    update olkdff f

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,4 @@ const whatsAppController = require("../controllers/whatsappControllers");
 router
 .get("/", whatsAppController.VerifyToken)
 .post("/", whatsAppController.ReceivedMessage)
-
 module.exports = router;
